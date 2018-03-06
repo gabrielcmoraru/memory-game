7937606313d17e3cbcd3b8459a4a00f0bf3d74c5
Gabriel C Moraru <gabrielcmoraru@gmail.com>
2018-03-06 01:00:28 +0000

    Added star rating, moves counter and css changes to the updates for star rating

--- js/app.js
+++ js/app.js
@@ -4,6 +4,15 @@
 // Spread HTMLCollection into an array
 let cards = [...document.getElementsByClassName('card')];
 
+// Selector for moves
+let displayMoves = document.querySelector('span');
+
+// Variable that stores number of moves
+let moves = 0;
+
+// Spread star elements into an array
+const starsRating = [...document.getElementsByClassName('fa-star')];
+
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -12,7 +21,7 @@ let cards = [...document.getElementsByClassName('card')];
  */
 
 function gameInit() {
-// Constant that holds all cards aka board
+// Variable that holds all cards aka board
 const board = document.querySelector('.deck');
 
 // Variable for shuffled cards
@@ -21,12 +30,6 @@ let mixCards = shuffle(cards);
 // Append each mixed card to the board
 // Set up the event listener for a card. If a card is clicked:
 // - display the card's symbol
-//
-// Advanced version using map, recomended to use the 'for loop' instead by mentor
-// mixCards.map(element => {
-// return board.append(element);
-// });
-//
 for (let i = 0; i < mixCards.length; i++) {
 	board.append(mixCards[i]);
 	cards[i].addEventListener('click', reveal);
@@ -43,9 +46,9 @@ function check() {
 	vs.push(this);
 	// While opened cards length is 2 check:
 	let len = vs.length;
-	if (len == 2){
+	if (len === 2) {
 		// While cards have same id call match function
-		if (vs[0].id === vs[1].id){
+		if (vs[0].id === vs[1].id) {
 			match();
 			// If not call noMatch function
 		} else {
@@ -55,22 +58,24 @@ console.log(vs)
 	}
 }
 
-// Opened cards match function add's match class, call reset match function with timer to allow card view for x ammount of ms
+// Opened cards match function add's 'match' class, calls moves counter and reset checker
 function match() {
 	vs[0].classList.add('match');
 	vs[1].classList.add('match');
-  resetMatch();
+	countMoves();
+	resetCheck();
 }
 
-// Opened cards noMatch function add's wrong class so you can view them (otherwise second card will not flip, it will for 1ms), call reset match function with timer to allow card view for x ammount of ms
+// Opened cards noMatch function add's 'wrong' class so you can view them (otherwise second card will not flip, it will for 1ms), call reset match function with timer to allow card view for x ammount of ms, and the moves counter
 function noMatch() {
 	vs[0].classList.add('wrong');
 	vs[1].classList.add('wrong');
-	setTimeout(resetMatch, 450);
+	countMoves();
+	setTimeout(resetCheck, 450);
 }
 
 // Remove classes from the 2 cards and empty the 'vs' array
-function resetMatch() {
+function resetCheck() {
 	vs[0].classList.remove('open', 'show', 'wrong');
 	vs[1].classList.remove('open', 'show', 'wrong');
 	vs = [];
@@ -80,6 +85,31 @@ function resetMatch() {
 function reveal() {
 	this.classList.toggle('open');
 	this.classList.toggle('show');
+}
+
+// Moves counter that updates the score panel and calls the star rating function
+function countMoves() {
+	moves++;
+	stars();
+	displayMoves.innerHTML = moves;
+}
+
+// Rates the current player according to the number of moves made, replacing stars with circles and different colours for different scoring systems
+function stars() {
+	switch (moves) {
+		case 9:
+		starsRating[0].classList.remove('fa-star');
+		starsRating[0].classList.add('fa-circle', 'minus');
+			break;
+		case 13:
+		starsRating[1].classList.remove('fa-star');
+		starsRating[1].classList.add('fa-circle', 'minus');
+			break;
+		case 18:
+		starsRating[2].classList.remove('fa-star');
+		starsRating[2].classList.add('fa-circle', 'minus');
+			break;
+	}
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
