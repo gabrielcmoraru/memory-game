6513727a3155416202dd834ab643fb09dce71fc0
Gabriel C Moraru <gabrielcmoraru@gmail.com>
2018-04-01 15:52:48 +0100

    Fixed comments

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-/*
- * Create a list that holds all of your cards
- */
 // Selector for individual card
 let card = document.getElementsByClassName('card');
 
@@ -34,13 +31,9 @@ let countup;
 // Variable that holds all cards aka board
 const board = document.querySelector('.deck');
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
+//Function to reset and initiate the game
+//--------------------
 function gameInit() {
 
 // Reset moves counter on page
@@ -350,20 +343,3 @@ document.addEventListener('DOMContentLoaded', gameInit());
 
 // Reset button
 restartGame.onclick = gameInit;
-
-
-
-
-
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
